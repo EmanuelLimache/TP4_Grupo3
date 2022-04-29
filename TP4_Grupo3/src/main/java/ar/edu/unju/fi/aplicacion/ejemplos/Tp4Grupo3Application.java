e010652af5ce70b8e1a2bf0e2eeabf5bd2b707eb
Enamuel L <Enamuel L@EmAnUeL>
2022-04-29 12:25:37 -0300

    primer commit

---
+++ TP4_Grupo3/src/main/java/ar/edu/unju/fi/aplicacion/ejemplos/Tp4Grupo3Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.aplicacion.ejemplos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4Grupo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4Grupo3Application.class, args);
+	}
+
+}
